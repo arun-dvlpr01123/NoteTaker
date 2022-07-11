5a062bae1370569f697a91d59357fcd16cda4837
ArunCan <aruncan@outlook.com>
2022-07-10 23:15:56 -0500

    Add project files.

---
+++ NoteTaker/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTaker
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+            loadcsvtoDataTable();
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns["Message"].Visible = false;
+
+
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text,txtMessage.Text);
+            txtTitle.Clear();
+            txtMessage.Clear();
+            savetoCSV(table);
+        }
+
+        private void savetoCSV(DataTable table)
+        {
+            String path = System.IO.Directory.GetCurrentDirectory() + "\\Notes.csv";
+
+
+            StreamWriter sw = new StreamWriter(File.Open(path, System.IO.FileMode.Create));
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                sw.Write(table.Columns[i]);
+                if (i < table.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < table.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
+
+        private void loadcsvtoDataTable()
+        {
+            String path = System.IO.Directory.GetCurrentDirectory() + "\\Notes.csv";
+            if (!File.Exists(path))
+            {
+                StreamWriter sw = new StreamWriter(File.Open(path, System.IO.FileMode.Create));
+                sw.WriteLine("Title,Message");
+                sw.Close ();
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    table.Columns.Add(header,typeof(String));
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = table.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    table.Rows.Add(dr);
+                }
+
+            }
+            
+        }
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+            
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            table.Rows[index].Delete();
+            txtTitle.Clear();
+            txtMessage.Clear();
+            savetoCSV(table);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
